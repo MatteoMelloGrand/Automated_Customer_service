f4d344040892bf27ec001b70c8d8fbc9ad736ec5
MatteoMelloGrand <75953711+MatteoMelloGrand@users.noreply.github.com>
2024-08-08 11:46:04 +0200

    Update app.py

--- Automated_customer_service/backend/app.py
+++ Automated_customer_service/backend/app.py
@@ -12,41 +12,52 @@ import os
 
 
 app = Flask(__name__)
-CORS(app)  # Allow Cross-Origin requests
+#CORS(app)  # Allow Cross-Origin requests
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 #API_TOKEN = "your_API_token" #?
 
-@app.route('/chat', methods = ['POST'])
+@app.route('/chat', methods = ['GET', 'POST'])
 def chat():
     input_data = request.json
     input_message = input_data.get('message', '')
-    with open('Input_LLM.txt','a+') as file: #reading the file and adding the question
-        file.write(f"Reply to the following client message: {input_message}\n")
-        file.seek(0)
-        content = file.read() #creating a variable with the text
-
+    with open('Input_LLM1.txt', 'r') as file:
+        original_content = file.read()
+    content1 = original_content + input_message 
+    #content1 = file.read() #creating a variable with the text
+    with open('Input_LLM.txt', 'w') as file:
+        file.write(original_content)
     client = Groq(
-    api_key=os.environ.get("GROQ_API_KEY"),
+    api_key="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",
 )
+
+
     try:
+
         chat_completion = client.chat.completions.create( 
         messages=[
             {
                 "role": "user",
-                "content": f"{content}",
+                "content": content1,
             }
         ],
-        model="llama3-groq-70b-8192",
+        model="llama-3.1-70b-versatile",
         )
         output = chat_completion.choices[0].message.content
         if output.endswith("..."):
             print("Response incomplete. Notifying customer service.")
+        
+
 
         return jsonify({'response': output})
             #write an email to the customer service with the question saying to enter the platform and reply as soon as possible
     except Exception as e:
         print(f"An error occured. Please try again. {e}")
-        return jsonify({'error': 'An error occurred. Please try again.'}), 500
+        return jsonify({'error': 'An error occurred. Please try again.'}), 500 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+    
